9ac5dcc44e767a5d97aaf4faf1ad942f2b136b70
Turki <w.sm@windowslive.com>
2022-12-05 14:19:53 +0300

    first commit

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction, Application } from "express";
+import path from "path";
+import cors from "cors";
+import fs from "fs";
+import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+const app: Application = express();
+dotenv.config();
+
+//** MIDDLEWARE **/
+app.use(bodyParser.raw({ type: "application/octet-stream", limit: "100mb" }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from the back");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on port ${process.env.PORT}`)
+);
