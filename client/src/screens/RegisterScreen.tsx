c64f8352e8ebbbd75f742af6bb8b8b0096b82bbe
Turki <w.sm@windowslive.com>
2022-12-18 15:39:07 +0300

    dynamic registration page based on the role of the user

--- client/src/screens/RegisterScreen.tsx
+++ client/src/screens/RegisterScreen.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Grid, Paper, Avatar, Typography } from "@material-ui/core";
+import { Grid, Paper, Avatar } from "@material-ui/core";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
@@ -14,6 +14,7 @@ import { InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import AuthContext from "../context/AuthContext";
 
 export default function Signup() {
+  const [role, setRole] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [fatherName, setFatherName] = useState<string>("");
   const [familyName, setFamilyName] = useState<string>("");
@@ -85,6 +86,36 @@ export default function Signup() {
           <h2 style={{ marginLeft: 350 }}>انشاء حساب</h2>
         </Grid>
         <form onSubmit={handleSubmit}>
+          <FormControl
+            component="fieldset"
+            style={{ marginTop: 10, textAlign: "right", width: "100%" }}
+          >
+            <FormLabel component="legend">مهنة</FormLabel>
+            <RadioGroup
+              name="role"
+              style={{ display: "initial" }}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setRole(e.target.value);
+              }}
+            >
+              <FormControlLabel
+                value="student"
+                control={<Radio />}
+                label="طالب"
+              />
+              <FormControlLabel
+                value="uniSupervisor"
+                control={<Radio />}
+                label="استاذ جامعي"
+              />
+              <FormControlLabel
+                value="companySupervisor"
+                control={<Radio />}
+                label="مسؤل بئة العمل"
+              />
+            </RadioGroup>
+          </FormControl>
+
           <TextField
             margin="normal"
             required
@@ -104,24 +135,27 @@ export default function Signup() {
               setName(e.target.value);
             }}
           />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="اسم الاب"
-            autoComplete="off"
-            autoFocus
-            placeholder="اسم الاب"
-            color="success"
-            InputProps={{
-              inputProps: {
-                style: { textAlign: "right" },
-              },
-            }}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              setFatherName(e.target.value);
-            }}
-          />
+
+          {role === "student" && (
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="اسم الاب"
+              autoComplete="off"
+              autoFocus
+              placeholder="اسم الاب"
+              color="success"
+              InputProps={{
+                inputProps: {
+                  style: { textAlign: "right" },
+                },
+              }}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setFatherName(e.target.value);
+              }}
+            />
+          )}
           <TextField
             margin="normal"
             required
@@ -207,92 +241,106 @@ export default function Signup() {
               },
             }}
           />
-          <InputLabel sx={{ textAlign: "right" }}>اهتمامك</InputLabel>
-          <Select
-            required
-            value={intrest}
-            color="success"
-            fullWidth
-            MenuProps={MenuProps}
-            sx={{ textAlign: "right" }}
-            labelId="demo-multiple-chip-label"
-            onChange={handleIntrest}
-          >
-            <MenuItem value={"طب"}>طب</MenuItem>
-            <MenuItem value={"قانون"}>قانون</MenuItem>
-            <MenuItem value={"هندسة"}>هندسة</MenuItem>
-            <MenuItem value={"تقنية المعلومات"}>تقنية المعلومات</MenuItem>
-            <MenuItem value={"العلوم"}>العلوم</MenuItem>
-            <MenuItem value={"اقتصاد"}>اقتصاد</MenuItem>
-            <MenuItem value={"ادارة الاعمال"}>ادارة الاعمال</MenuItem>
-            <MenuItem value={"تسويق"}>تسويق</MenuItem>
-            <MenuItem value={"محاسبة"}>محاسبة</MenuItem>
-            <MenuItem value={"شريعة"}>شريعة</MenuItem>
-            <MenuItem value={"لغات و ترجمة"}>لغات و ترجمة</MenuItem>
-            <MenuItem value={"اعلام"}>اعلام</MenuItem>
-            <MenuItem value={"صحافة"}>صحافة</MenuItem>
-            <MenuItem value={"الزراعة والبيئة"}>الزراعة والبيئة</MenuItem>
-          </Select>
-          <InputLabel sx={{ textAlign: "right" }}>التخصص</InputLabel>
-          <Select
-            required
-            value={major}
-            color="success"
-            fullWidth
-            MenuProps={MenuProps}
-            sx={{ textAlign: "right" }}
-            onChange={handleMajor}
-          >
-            <MenuItem value={"طب"}>طب</MenuItem>
-            <MenuItem value={"قانون"}>قانون</MenuItem>
-            <MenuItem value={"هندسة"}>هندسة</MenuItem>
-            <MenuItem value={"تقنية المعلومات"}>تقنية المعلومات</MenuItem>
-            <MenuItem value={"العلوم"}>العلوم</MenuItem>
-            <MenuItem value={"اقتصاد"}>اقتصاد</MenuItem>
-            <MenuItem value={"ادارة الاعمال"}>ادارة الاعمال</MenuItem>
-            <MenuItem value={"تسويق"}>تسويق</MenuItem>
-            <MenuItem value={"محاسبة"}>محاسبة</MenuItem>
-            <MenuItem value={"شريعة"}>شريعة</MenuItem>
-            <MenuItem value={"لغات و ترجمة"}>لغات و ترجمة</MenuItem>
-            <MenuItem value={"اعلام"}>اعلام</MenuItem>
-            <MenuItem value={"صحافة"}>صحافة</MenuItem>
-            <MenuItem value={"الزراعة والبيئة"}>الزراعة والبيئة</MenuItem>
-          </Select>
-          <InputLabel sx={{ textAlign: "right" }}>الجامعة</InputLabel>
-          <Select
-            required
-            value={university}
-            color="success"
-            fullWidth
-            MenuProps={MenuProps}
-            sx={{ textAlign: "right" }}
-            onChange={handleUniversity}
-          >
-            <MenuItem value={"جامعة الامام محمد بن سعود"}>
-              جامعة الامام محمد بن سعود
-            </MenuItem>
-            <MenuItem value={"جامعة الملك سعود"}>جامعة الملك سعود</MenuItem>
-            <MenuItem value={"جامعة الاميرة نوره"}>جامعة الاميرة نوره</MenuItem>
-            <MenuItem value={"تقنية المعلومات"}>جامعة ام القرى</MenuItem>
-            <MenuItem value={"العلوم"}>جامعة الملك عبدالعزيز</MenuItem>
-            <MenuItem value={"اقتصاد"}>
-              جامعة الملك فهد لي البترول و المعادن
-            </MenuItem>
-            <MenuItem value={"ادارة الاعمال"}>جامعة الملك فيصل</MenuItem>
-            <MenuItem value={"تسويق"}>جامعة القصيم</MenuItem>
-            <MenuItem value={"محاسبة"}>جامعة الملك خالد</MenuItem>
-            <MenuItem value={"شريعة"}>جامعة حائل</MenuItem>
-            <MenuItem value={"لغات و ترجمة"}>جامعة الطايف</MenuItem>
-            <MenuItem value={"اعلام"}>جامعة تبوك</MenuItem>
-            <MenuItem value={"صحافة"}>جامعة الامير سطام</MenuItem>
-            <MenuItem value={"الزراعة والبيئة"}>
-              جامعة الامير سلطان بن عبدالعزيز
-            </MenuItem>
-            <MenuItem value={"صحافة"}>جامعة الجبيل الصناعية</MenuItem>
-            <MenuItem value={"صحافة"}>جامعة ينبع الصناعية</MenuItem>
-            <MenuItem value={"صحافة"}>جامعة الامير سطام</MenuItem>
-            <MenuItem value={"صحافة"}>جامعة الامير سطام</MenuItem>
-          </Select>
+          {role === "student" && (
+            <>
+              <InputLabel sx={{ textAlign: "right" }}>اهتمامك</InputLabel>
+              <Select
+                required
+                value={intrest}
+                color="success"
+                fullWidth
+                MenuProps={MenuProps}
+                sx={{ textAlign: "right" }}
+                labelId="demo-multiple-chip-label"
+                onChange={handleIntrest}
+              >
+                <MenuItem value={"طب"}>طب</MenuItem>
+                <MenuItem value={"قانون"}>قانون</MenuItem>
+                <MenuItem value={"هندسة"}>هندسة</MenuItem>
+                <MenuItem value={"تقنية المعلومات"}>تقنية المعلومات</MenuItem>
+                <MenuItem value={"العلوم"}>العلوم</MenuItem>
+                <MenuItem value={"اقتصاد"}>اقتصاد</MenuItem>
+                <MenuItem value={"ادارة الاعمال"}>ادارة الاعمال</MenuItem>
+                <MenuItem value={"تسويق"}>تسويق</MenuItem>
+                <MenuItem value={"محاسبة"}>محاسبة</MenuItem>
+                <MenuItem value={"شريعة"}>شريعة</MenuItem>
+                <MenuItem value={"لغات و ترجمة"}>لغات و ترجمة</MenuItem>
+                <MenuItem value={"اعلام"}>اعلام</MenuItem>
+                <MenuItem value={"صحافة"}>صحافة</MenuItem>
+                <MenuItem value={"الزراعة والبيئة"}>الزراعة والبيئة</MenuItem>
+              </Select>
+            </>
+          )}
+          {role === "student" && (
+            <>
+              <InputLabel sx={{ textAlign: "right" }}>التخصص</InputLabel>
+              <Select
+                required
+                value={major}
+                color="success"
+                fullWidth
+                MenuProps={MenuProps}
+                sx={{ textAlign: "right" }}
+                onChange={handleMajor}
+              >
+                <MenuItem value={"طب"}>طب</MenuItem>
+                <MenuItem value={"قانون"}>قانون</MenuItem>
+                <MenuItem value={"هندسة"}>هندسة</MenuItem>
+                <MenuItem value={"تقنية المعلومات"}>تقنية المعلومات</MenuItem>
+                <MenuItem value={"العلوم"}>العلوم</MenuItem>
+                <MenuItem value={"اقتصاد"}>اقتصاد</MenuItem>
+                <MenuItem value={"ادارة الاعمال"}>ادارة الاعمال</MenuItem>
+                <MenuItem value={"تسويق"}>تسويق</MenuItem>
+                <MenuItem value={"محاسبة"}>محاسبة</MenuItem>
+                <MenuItem value={"شريعة"}>شريعة</MenuItem>
+                <MenuItem value={"لغات و ترجمة"}>لغات و ترجمة</MenuItem>
+                <MenuItem value={"اعلام"}>اعلام</MenuItem>
+                <MenuItem value={"صحافة"}>صحافة</MenuItem>
+                <MenuItem value={"الزراعة والبيئة"}>الزراعة والبيئة</MenuItem>
+              </Select>
+            </>
+          )}
+          {(role === "student" || role === "uniSupervisor") && (
+            <>
+              <InputLabel sx={{ textAlign: "right" }}>الجامعة</InputLabel>
+              <Select
+                required
+                value={university}
+                color="success"
+                fullWidth
+                MenuProps={MenuProps}
+                sx={{ textAlign: "right" }}
+                onChange={handleUniversity}
+              >
+                <MenuItem value={"جامعة الامام محمد بن سعود"}>
+                  جامعة الامام محمد بن سعود
+                </MenuItem>
+                <MenuItem value={"جامعة الملك سعود"}>جامعة الملك سعود</MenuItem>
+                <MenuItem value={"جامعة الاميرة نوره"}>
+                  جامعة الاميرة نوره
+                </MenuItem>
+                <MenuItem value={"تقنية المعلومات"}>جامعة ام القرى</MenuItem>
+                <MenuItem value={"العلوم"}>جامعة الملك عبدالعزيز</MenuItem>
+                <MenuItem value={"اقتصاد"}>
+                  جامعة الملك فهد لي البترول و المعادن
+                </MenuItem>
+                <MenuItem value={"ادارة الاعمال"}>جامعة الملك فيصل</MenuItem>
+                <MenuItem value={"تسويق"}>جامعة القصيم</MenuItem>
+                <MenuItem value={"محاسبة"}>جامعة الملك خالد</MenuItem>
+                <MenuItem value={"شريعة"}>جامعة حائل</MenuItem>
+                <MenuItem value={"لغات و ترجمة"}>جامعة الطايف</MenuItem>
+                <MenuItem value={"اعلام"}>جامعة تبوك</MenuItem>
+                <MenuItem value={"صحافة"}>جامعة الامير سطام</MenuItem>
+                <MenuItem value={"الزراعة والبيئة"}>
+                  جامعة الامير سلطان بن عبدالعزيز
+                </MenuItem>
+                <MenuItem value={"صحافة"}>جامعة الجبيل الصناعية</MenuItem>
+                <MenuItem value={"صحافة"}>جامعة ينبع الصناعية</MenuItem>
+                <MenuItem value={"صحافة"}>جامعة الامير سطام</MenuItem>
+                <MenuItem value={"صحافة"}>جامعة الامير سطام</MenuItem>
+              </Select>
+            </>
+          )}
           <FormControlLabel
             control={<Checkbox name="checkedA" />}
             label="اقبل بقوانين المنصه"
