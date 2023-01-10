ea6be3df22b9d9d4abb214a21868bbbb793716f0
Turki <w.sm@windowslive.com>
2023-01-10 16:26:41 +0300

    dialog implmentation

--- client/src/screens/CompanyListScreen.tsx
+++ client/src/screens/CompanyListScreen.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { Box } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import ApartmentIcon from "@mui/icons-material/Apartment";
+import { Box, Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 type companyList = {
   field: string;
@@ -15,6 +18,11 @@ type companyList = {
 
 export default function CompanyListScreen() {
   const [companyList, setCompanyList] = useState([{} as companyList]);
+  const [selectedCompanyName, setSelectedCompanyName] = useState("");
+  const [selectedCompanyField, setSelectedCompanyField] = useState("");
+  const [selectedCompanyLocation, setSelectedCompanyLocation] = useState("");
+  const [selectedCompanyType, setSelectedCompanyType] = useState("");
+  const [open, setOpen] = useState(false);
 
   const colums = useMemo(
     () => [
@@ -53,26 +61,58 @@ export default function CompanyListScreen() {
   }, []);
 
   return (
-    <Box
-      component={"div"}
-      sx={{
-        height: 750,
-        width: "81%",
-        marginTop: 8,
+    <>
+      <Box
+        component={"div"}
+        sx={{
+          height: 750,
+          width: "81%",
+          marginTop: 9,
 
-        backgroundColor: "#EFF5F5",
-      }}
-    >
-      <DataGrid
-        onRowClick={(e: any) => {
-          console.log(e.row.name);
+          backgroundColor: "#EFF5F5",
         }}
-        pageSize={12}
-        columns={colums}
-        rows={companyList}
-        getRowId={(row) => row.id + 1}
-        sx={{ fontSize: 20, margin: 2 }}
-      />
-    </Box>
+      >
+        <DataGrid
+          onRowClick={(e: any) => {
+            setSelectedCompanyName(e.row.name);
+            setSelectedCompanyField(e.row.field);
+            setSelectedCompanyLocation(e.row.location);
+            setSelectedCompanyType(e.row.type);
+            setOpen(true);
+          }}
+          pageSize={12}
+          columns={colums}
+          rows={companyList}
+          getRowId={(row) => row.id + 1}
+          sx={{ fontSize: 20, margin: 2 }}
+        />
+      </Box>
+      <Dialog fullWidth open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>تقديم على الجهة</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{selectedCompanyName}</DialogContentText>
+          <DialogContentText>{selectedCompanyField}</DialogContentText>
+          <DialogContentText>{selectedCompanyType}</DialogContentText>
+          <DialogContentText>{selectedCompanyLocation}</DialogContentText>
+          <Box></Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            sx={{
+              fontSize: 20,
+              width: "100%",
+              marginBottom: 3,
+              backgroundColor: "#3C6255",
+              "&:hover": {
+                backgroundColor: "#86C8BC",
+              },
+            }}
+          >
+            قدم
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
