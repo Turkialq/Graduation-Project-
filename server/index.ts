adface690436e4f485e6029c94b578bec95a1950
Turki <w.sm@windowslive.com>
2023-01-20 14:38:37 +0300

    SSL cert (HTTPS) server implementation

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,27 @@
 import express, { Request, Response, Application } from "express";
 import cors from "cors";
+import https from "https";
+import fs from "fs";
+import path from "path";
 import * as dotenv from "dotenv";
 import bodyParser from "body-parser";
 // Routes
 import companyRouter from "./routes/companyRoutes";
 import userRouter from "./routes/userAuth";
 import studentRouter from "./routes/studentSupervisor";
+
 //** CONFIG **/
 const app: Application = express();
 dotenv.config();
+
+//** SSL_SERVER **//
+const SSL_SERVER = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app
+);
 
 //** MIDDLEWARE **/
 app.use(bodyParser.json());
@@ -26,6 +39,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from the back");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server started on port ${process.env.PORT}`)
-);
+SSL_SERVER.listen(process.env.PORT, () => {
+  console.log(`secure server on port 8081`);
+});
