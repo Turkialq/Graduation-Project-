80cda49d2fb5e3bbf98dcbc6bdc2eb04b9b55a2b
amhalrasheed <71130732+amhalrasheed@users.noreply.github.com>
2023-02-10 03:17:17 +0300

    Revert "add recommendation system api"
    This reverts commit fe27b3a29f0b879e9dbcc2a63ed9aa46fc3f7437.

--- rsAPI/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from classifier.recommender import api_call
-
-
-@api_view(["post"])
-def recommendation(request):
-
-    return Response(api_call(request.body))
